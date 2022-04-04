95bb2dce54be842eccd390793cf06f5a3e631abc
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-04 14:36:59 +0530

    Update codecheckout.groovy

--- vars/codecheckout.groovy
+++ vars/codecheckout.groovy
@@ -1,4 +1,4 @@
-def call(String username = 'null') {
+/*def call(String username = 'null') {
 	echo "This is shared library greetUser: ${username}"
 	pipeline {
 		environment {
@@ -9,6 +9,22 @@ def call(String username = 'null') {
 			stage('SHARED-STAGE') {
 				steps {
 					echo "Hi $USER_NAME, How are you !"
+				}	
+			}
+		}
+	}
+} */
+def call(String branch = 'null') {
+	echo "This is shared library codecheckout: ${branch}"
+	pipeline {
+		environment {
+			BRANCH_NAME = "${branch}"
+		}
+		agent any 
+		stages {
+			stage('CODECHECKOUT from SCM') {
+				steps {
+					echo "You have chosen branch $BRANCH_NAME"
 				}	
 			}
 		}
