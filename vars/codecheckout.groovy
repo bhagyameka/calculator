f377c1ae5977838a2481c8605fd1b309e665ea0b
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-04 16:37:29 +0530

    Update codecheckout.groovy

--- vars/codecheckout.groovy
+++ vars/codecheckout.groovy
@@ -25,7 +25,7 @@ def call(String branch = 'null') {
 			stage('CODECHECKOUT from SCM') {
 				steps {
 					echo "You have chosen branch $BRANCH_NAME"
-					git branch: '"$BRANCH_NAME"', url: "https://github.com/bhagyameka/calculator.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
+					git branch: "$BRANCH_NAME", url: "https://github.com/bhagyameka/calculator.git", credentialsId: 'c6947d68-906d-4126-a88a-d93c8d4a1ec8'
 				}	
 			}
 		        stage('build') {
