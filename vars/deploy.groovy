58fe6c0c6d8cebe3c248dd95da6fd1a1621662f5
bhagyameka <bhagyakakumanu@gmail.com>
2022-04-04 22:55:32 +0530

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,44 @@
+def call(String branch = 'null') {
+	echo "This is shared library deploy: ${branch}"
+pipeline {
+     environment {
+			DEPLOY_TO = "${branch}"
+		}
+agent any
+
+stages {
+
+
+ stage ('Download from Jfrog') {
+      steps {
+          echo '*******download from JFrog start*******'
+       /*   rtDownload (
+                   serverId: 'artifactory_idfcbank',
+                   spec: '''{
+                           "files": [
+                            {
+                               "pattern": "maven-local/idfc/digital_onboarding/master/",
+                               "target": "D:/"
+                             }
+                          ]
+                         }''',
+             ) */
+          echo '*******download from JFrog End*******'
+      }
+ }
+
+
+        
+ stage ('Deploy on Weblogic') {
+      
+      steps {
+          echo '*******deploy on weblogic Start to $DEPLOY_TO *******'
+         //  sshagent(['ID_WEBLOGIC_DIGITAL_ONBOARDING']) {
+          // sh "scp -v -o StrictHostKeyChecking=no /data/jenkins/workspace/Digital Onboarding/DemoPipelineAsCode/target/*.war deployer@10.5.25.170:7001/weblogicdomain/onboardapp/servers/AdminServer/upload/sample1.war/app/"
+          echo '*******deploy on weblogic done*******'
+        // }
+      }
+ } 
+}
+}
+}
